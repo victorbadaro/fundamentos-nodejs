1132e7b55b0046a7b3bddfaa8b6df975259fda99
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-04-28 16:19:45 -0300

    feat: add the first method in the api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen();
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Hello, World!'
+  });
+});
+
+app.listen(3333);
